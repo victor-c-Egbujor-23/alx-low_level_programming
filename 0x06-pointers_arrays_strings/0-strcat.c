3e79a91c180b853f7b86b875e90c6461c2dd3ff3
victor-c-Egbujor-23 <dozybrown2015@yahoo.com>
2023-03-03 16:39:33 -0800

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,18 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0
-	while (dest [i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	i = 0;
 	while (src[j] != "\0")
 		dest[i] = src[j];
-                i++;
+		i++;
 		j++;
 	}
 
-        dest[i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 
